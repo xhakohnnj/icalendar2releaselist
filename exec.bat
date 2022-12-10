1862bcb471c003499c22d267c8e4dfb3d03a5afb
xhakohnnj <xhakohnnj@gmail.com>
2022-12-11 00:01:24 +0900

    日付を最新の範囲に変更

--- exec.bat
+++ exec.bat
@@ -1,6 +1,6 @@
 echo off
 
-py generateTennpure.py output.txt 2022/11/24 2023/03/31
+py generateTennpure.py output.txt 2022/12/04 2023/03/31
 rem py generateTennpureWithFile.py output.txt release.ics gamepass_in.ics gamepass_out.ics 2022/11/22 2023/03/31
 
 pause
